ff0452db571adbadce928fd22e6eb79fd30999a3
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-29 13:57:59 -0600

    Add: serializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
-  it 'sends a list of all merchants' do
+  it 'gets all merchants' do
     create_list(:merchant, 13)
 
     get '/api/v1/merchants'
@@ -9,15 +9,23 @@ RSpec.describe 'Merchants API' do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants[:data].count).to eq(13)
 
-    expect(merchants.count).to eq(13)
-
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_a(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:type]).to eq("merchant")
+
+      expect(merchant[:attributes]).to have_key(:id)
+      expect(merchant[:attributes][:id]).to be_a(Integer)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'responds with all the attributes of one merchant' do
+
   end
 end
