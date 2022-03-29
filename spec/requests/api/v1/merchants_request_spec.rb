d06d3d9c35084dbca14c522952a8cb07320c7513
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-29 14:26:34 -0600

    Add: merchant show endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -17,15 +17,29 @@ RSpec.describe 'Merchants API' do
 
       expect(merchant[:type]).to eq("merchant")
 
-      expect(merchant[:attributes]).to have_key(:id)
-      expect(merchant[:attributes][:id]).to be_a(Integer)
-
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 
-  it 'responds with all the attributes of one merchant' do
+  it 'gets one merchant' do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
 
+    get "/api/v1/merchants/#{merchant_1.id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data].count).to eq(3)
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to be_a(String)
+
+    expect(merchant[:data][:type]).to eq("merchant")
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 end
