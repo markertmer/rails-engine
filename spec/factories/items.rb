7087e43ddb95dbfbf8c896432c06e36fac9021aa
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-29 19:07:37 -0600

    Remove: merchant_id from factory bot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     name { Faker::Lorem.word }
     description { Faker::ChuckNorris.fact }
     unit_price { Faker::Number.decimal(l_digits:2) }
-    merchant_id { Faker::Number.between(from: 1, to: 1000) }
   end
 end
