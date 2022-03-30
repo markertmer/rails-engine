094f0eb50e7fc61b009acd69ee3001d963856517
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-29 18:11:43 -0600

    Refactor: routes & controller directory

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ItemsController < ApplicationController
+class Api::V1::Merchants::ItemsController < ApplicationController
   def index
     # render json: Merchant.all
     merchant = Merchant.find(params[:merchant_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
+      namespace :merchants do
+        get '', to: 'merchants#index'
+        get ':id', to: 'merchants#show'
+        get ':merchant_id/items', to: 'items#index'
+        # resources :merchants, only: [:index, :show] do
+        #   resources :items, only: [:index]
+        # end
+        # resources :items, only: [:index]
+      end
+      namespace :items do
+        get '', to: 'items#index'
       end
     end
   end
