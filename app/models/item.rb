95a74a4d7478e6f7f7d3af35a8b2bc3e5f64e535
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-31 17:28:11 -0600

    Add: item search methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.find_by_name(keyword)
+    where("name ILIKE ?", "%#{keyword}%").order(:name)
+  end
+
+  def self.find_by_price(price_hash)
+    floor = 0
+    floor = price_hash[:min] if price_hash[:min]
+    ceiling = Float::INFINITY
+    ceiling = price_hash[:max] if price_hash[:max]
+
+    where("unit_price >= ?", "#{floor}")
+    .where("unit_price <= ?", "#{ceiling}")
+    .order(:unit_price)
+  end
 end
