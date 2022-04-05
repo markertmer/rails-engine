5e54e6f790355c80a06972b9072db5662e976b73
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-05 14:19:34 -0600

    Add: model relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+
   validates_associated :merchant
   validates :name, presence: true
   validates :description, presence: true
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,34 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
 
   validates :name, presence: true
 
   def self.find_by_name(keyword)
     where("name ILIKE ?", "%#{keyword}%").order(:name)
   end
+
+  def self.top_merchants_by_revenue(number)
+    # Merchant.joins(invoices: { invoice_items: :transactions })
+    joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
+    .group('merchants.id')
+    .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) as total_revenue')
+    .order('total_revenue DESC')
+    .limit(number)
+    # .order(total_revenue: :descending)
+  end
+
+  def self.most_items(number)
+    joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
+    .group('merchants.id')
+    .select('merchants.*, SUM(invoice_items.quantity) as total_sold')
+    .order('total_sold DESC')
+    .limit(number)
+  end
+
 end
